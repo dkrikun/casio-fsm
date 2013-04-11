7debee88dd00e7f5718473ca6dc5aabe38f13807
danielkr <krikun.daniel@gmail.com>
2013-04-11 22:09:29 +0300

    Remove vim & git setup from VM provision

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,11 +5,12 @@
 # mv smc_6_1_0 foreign/smc_6_1_0
 
 
-# set up git and vim
 apt-get update
 
-apt-get install -y git
-apt-get install -y vim
+# set up git and vim -- not necessary, code editing & git takes place outside the vm
+# apt-get install -y git
+# apt-get install -y vim
+# 
+# git config --global core.editor vim
+# git config --global color.ui = true
 
-git config --global core.editor vim
-git config --global color.ui = true
