7dfc115aaa5fa22ce18d25239e01d1f9d68034ef
danielkr <krikun.daniel@gmail.com>
2013-04-20 18:21:02 +0300

    Move sched impl to cpp file

--- src/Sched.cpp
+++ src/Sched.cpp
@@ -1,1 +1,35 @@
 #include <Sched.h>
+
+#include <unistd.h>	// for sleep()
+#include <iostream>
+
+Sched::Sched(Input* input, Watch* watch, Display* display)
+	: input_(input)
+	, watch_(watch)
+	, display_(display)
+{}
+
+void Sched::run()
+{
+	unsigned int period_counter = 0;
+	while(!shouldStop())
+	{
+		const unsigned int period_usec = 333333;
+		usleep(period_usec);
+
+		// clear screen, normally this should be done in Display
+		// but we want to fsm debug output to be shown (and not wiped away by this
+		// clear screen)
+		display_->clearScreen();
+
+		// check input each 33.3 msec
+		input_->frame();
+
+		// tick the clock once per sec
+		if(period_counter++ == 2)
+		{
+			watch_->frame();
+			period_counter = 0;
+		}
+	}
+}
--- src/Sched.h
+++ src/Sched.h
@@ -5,43 +5,13 @@
 #include <Watch.h>
 #include <Display.h>
 
-#include <unistd.h>	// for sleep()
-#include <iostream>
-
 class Sched
 {
 	public:
-	Sched(Input* input, Watch* watch, Display* display)
-		: input_(input)
-		, watch_(watch)
-		, display_(display)
-	{}
+	Sched(Input* input, Watch* watch, Display* display);
 
 	public:
-	void run()
-	{
-		unsigned int period_counter = 0;
-		while(!shouldStop())
-		{
-			const unsigned int period_usec = 333333;
-			usleep(period_usec);
-
-			// clear screen, normally this should be done in Display
-			// but we want to fsm debug output to be shown (and not wiped away by this
-			// clear screen)
-			display_->clearScreen();
-
-			// check input each 33.3 msec
-			input_->frame();
-
-			// tick the clock once per sec
-			if(period_counter++ == 2)
-			{
-				watch_->frame();
-				period_counter = 0;
-			}
-		}
-	}
+	void run();
 
 	bool shouldStop() const
 	{ return input_->shouldStop(); }
